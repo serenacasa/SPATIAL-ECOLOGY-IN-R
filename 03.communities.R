9b1fbfb708e6396e4c17cd500e0816d8bac24a18
Serena Casagrande <147078033+serenacasa@users.noreply.github.com>
2023-11-14 15:05:29 +0100

    Create 03.communities.R

---
+++ 03.communities.R
@@ -0,0 +1,46 @@
+library(vegan) 
+
+data(dune)
+head(dune)
+
+ord <- decorana(dune)
+
+ldc1 =  3.7004 
+ldc2 =  3.1166 
+ldc3 = 1.30055
+ldc4 = 1.47888
+
+total = ldc1 + ldc2 + ldc3 + ldc4
+
+pldc1 = ldc1 * 100 / total
+pldc2 = ldc2 * 100 / total
+pldc3 = ldc3 * 100 / total
+pldc4 = ldc4 * 100 / total
+
+pldc1
+pldc2
+
+pldc1 + pldc2
+plot(ord) 
+
+# relation among species in time
+library(overlap)
+
+data(kerinci)
+summary(kerinci)
+
+kerinci$timeRad <- kerinci$Time * 2 * pi
+
+tiger <- kerinci[kerinci$Sps=="tiger",]
+
+timetig <- tiger$timeRad
+densityPlot(timetig, rug=TRUE)
+
+# exercise: to select only the data on macaque individuals:
+macaque <- kerinci[kerinci$Sps=="macaque",]
+head(macaque)
+
+timemac <- macaque$timeRad
+densityPlot(timemac, rug=TRUE)
+overlapPlot(timetig, timemac)
+
