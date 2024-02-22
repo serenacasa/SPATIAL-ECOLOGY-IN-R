366c13446b47be8a0470847047620b48fd1943e3
Serena Casagrande <147078033+serenacasa@users.noreply.github.com>
2024-02-22 17:52:31 +0100

    Update and rename colorblindr.R to 12.colorblindr.R

---
+++ 12.colorblindr.R
@@ -0,0 +1,16 @@
+#simulating colour blind vision
+
+library(devtools)
+devtools::install_github("clauswilke/colorblindr")
+                                                              #then we can recall the package from the library function
+library(colorblindr)
+library(ggplot2)                                              #a library to build additional graphics
+
+iris
+                                                              #build a ggplot with the iris aestethics
+fig <-ggplot(iris, aes(Sepal.Length, fill= Species)) + geom_density(alpha=0.7)
+fig
+                                                              #color vision deficiency
+cvd_grid(fig)
+
+
