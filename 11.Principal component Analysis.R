a58710578f2154bae59273b721a16b19ceaff4e5
Serena Casagrande <147078033+serenacasa@users.noreply.github.com>
2024-02-23 16:30:18 +0100

    Update 11.Principal component Analysis.R

--- 11.Principal component Analysis.R
+++ 11.Principal component Analysis.R
@@ -1,4 +1,4 @@
-### Principal Component Analysis ###
+#### PRINCIPAL COMPONENT ANALYSIS ####
 
 library(imageRy)
 library(terra)
@@ -10,14 +10,14 @@ sent <- im.import("sentinel.png")
 
 pairs(sent)
 
-# perform PCA on sent
+                                                                  # we perform PCA on sent using the function im.pca()
 sentpc <- im.pca(sent)
 pc1 <- sentpc$PC1
 
 viridisc <- colorRampPalette(viridis(7))(255)
 plot(pc1, col=viridisc)
 
-# calculating standard deviation ontop of pc1
+                                                                  # calculating standard deviation ontop of pc1
 pc1sd3 <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
 plot(pc1sd3, col=viridisc)
 
@@ -26,14 +26,14 @@ plot(pc1sd7, col=viridisc)
 
 par(mfrow=c(2,3))
 im.plotRGB(sent, 2, 1, 3)
-# sd from the variability script:
+                                                                  # sd from the variability script.
 plot(sd3, col=viridisc)
 plot(sd7, col=viridisc)
 plot(pc1, col=viridisc)
 plot(pc1sd3, col=viridisc)
 plot(pc1sd7, col=viridisc)
 
-# stack all the standard deviation layers
+                                                                  # then we stack all the standard deviation layers
 sdstack <- c(sd3, sd7, pc1sd3, pc1sd7)
 names(sdstack) <- c("sd3", "sd7", "pc1sd3", "pc1sd7")
 plot(sdstack, col=viridisc)
