79a78d6193b9cb588158e99111c60c9aa3ec6e71
Serena Casagrande <147078033+serenacasa@users.noreply.github.com>
2025-09-10 16:09:39 +0200

    Update 02.Populations.R

--- 02.Populations.R
+++ 02.Populations.R
@@ -1,76 +1,76 @@
-### Code related to population ecology
+### Population ecology
 
-#a package is needed for point pattern analysis
+    # the package spatstat is needed for point pattern analysis
 install.packages("spatstat")
 library(statstat)
 
-#let's use the bei data:
-#data description
-#http://CRAN.R-project.org/
+    # let's use the bei data:
+    # data description
+    # http://CRAN.R-project.org/
 
 bei
 
-#plotting data
+    # plotting data using the "plot" function:
 plot(bei)
 
-#change the dimension (,cex=)
+    # to change the dimension of the dots we use (, cex=):
 plot(bei, cex=.2)
 
-#changing the symbol we use -pch
+    # to change the symbol we use (, pch=):
 plot(bei, cex=.2, pch=19)
 
-#additional datasets
+    # additional datasets:
 bei.extra
 plot(bei.extra)
 
-#let's use only part of the dataset: -> elev
+    # to use only part of the dataset, we use $elev for the data regarding elevation:
 plot(bei.extra$elev)
 elevation <- bei.extra$elev
 plot(elevation)
 
-#second method to select elements
+    # second method to select elements:
 elevation2 <- bei.extra[[1]]
 plot(elevation2)
 
-#passing from points to a continuous surface
+    # passing from points to a continuous surface
 densitymap <- density(bei)
-plot(densitymap)                                                             #this permits us to have the colored map
+plot(densitymap)             
+    # this permits us to have the colored map
 
-                                                                             #The function POINTS to insert the points in the bei dataset 
-points(bei, cex=.2)                                                          #on top of the colored density map.
-                                                                             #Avoid using maps with blue, green and red together 
-                                                                             #due to colorblindness
+    # the "points" function to insert the points in the bei dataset on top of the colored density map:
+points(bei, cex=.2)                                                                                                                                       
+    # avoid using maps with blue, green, and red together due to colorblindness
 
-#to change the color of the map by using "colorRampPalette" function:
+    # change the color of the map by using "colorRampPalette" function:
 colorRampPalette(c("black", "red", "orange", "yellow"))(100)
 
 c1 <- colorRampPalette(c("black", "red", "orange", "yellow"))(100)
 plot(densitymap, col=c1)
+    # the yellow color impacts our eyes the most, so it is to be used for the high part of the data
 
-                                                                           #the yellow color is the color that impacts the most our eye, 
-                                                                           #so it is to be used for the high part of the data
-cl<- colorRampPalette(c("black","red", "orange", "yellow"))(4)
+cl<- colorRampPalette(c("black", "red", "orange", "yellow"))(4)
 plot(densitymap, col=cl)
-
-                                                                           #to see which colors can be used in R we just have to search it on the internet
+    # to see which colors can be used in R, we just have to search it on the internet
 
 c2 <- colorRampPalette(c("black","darkcyan","cyan3","cyan1"))(50)
 plot(densitymap, col=c2)
-                                                                           #NEVER USE TURBO COLOR PALETTE/RAINBOW PALETTE 
+    # never use turbo color palette or the rainbow palette 
 
-plot(bei.extra)                                                            #these are more variables
-                                                                           #to select the first element of bei.extra
+plot(bei.extra)                                             
+    # these are more variables
+
+    # to select the first element of bei.extra:
 elev<- bei.extra[[1]]
 plot(elev)
 
 
-                                                                           #the dollar symbol $ is used to extract elements
-elev <- bei.extra$elev
+elev <- bei.extra$elev 
+    # the dollar symbol $ is used to extract elements
 plot(elev)
 
-                                                                           #In constructing MULTIFRAMES (mf) 
-                                                                           #we will have to state how many rows and columns we want.
-par(mfrow=1,2)                                                             #first create the multiframe with this command
+                                                                           # In constructing MULTIFRAMES (mf) 
+                                                                           # we will have to state how many rows and columns we want.
+par(mfrow=1,2)                                                             # first create the multiframe with this command
                                                                            
 plot(densitymap)                                                           #then plot what we want inside the multiframe that we created
 plot(elev)                                                                 #also plotted to be inside our multiframe
